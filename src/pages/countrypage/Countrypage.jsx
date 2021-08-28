667116a6501ff7a39545a7e75fd17d6e8d6ea83c
Laitas <61273117+Laitas@users.noreply.github.com>
2021-08-28 16:25:56 +0300

    added styling

--- src/pages/countrypage/Countrypage.jsx
+++ src/pages/countrypage/Countrypage.jsx
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import {useSelector} from 'react-redux'
+import './Countrypage.scss'
 
 const Countrypage = () => {
     const [loading,setLoading] = useState(true)
@@ -32,40 +33,69 @@ const Countrypage = () => {
         console.log(borders);
         console.log(languages);
         return (
-            <div className='country-page'>
-                <div className="place">
-                    <div className="flag">
-                        <img src={flag} alt={name} />
-                    </div>
-                    <div className="info">
-                        <div className="main-info">
-                        <h1>{name}</h1>
-                        <ul>
-                            <li><span>Native Name: </span>{nativeName}</li>
-                            <li><span>Population: </span>{population.toLocaleString()}</li>
-                            <li><span>Region: </span>{region}</li>
-                            <li><span>Sub Region: </span>{subregion}</li>
-                            <li><span>Capital: </span>{capital}</li>
-                            <li><span>Top Level Domain: </span>{topLevelDomain}</li>
-                            <li><span>Currency: {currencies[0].name}</span>
-                            </li>
-                            <li><span>Languages: </span>{languages.map(language =>{
-                                let list = []
-                                list.push(`${language.name} `)
-                                return list
-                            })}</li>
-                        </ul>
+          <div className="country-page">
+            <div className="place">
+              <div className="flag">
+                <img src={flag} alt={name} />
+              </div>
+              <div className="info">
+                  <h1>{name}</h1>
+                <div className="main-info">
+                  <div className="main-info--top">
+                  <ul>
+                    <li>
+                      <span>Native Name: </span>
+                      {nativeName}
+                    </li>
+                    <li>
+                      <span>Population: </span>
+                      {population.toLocaleString()}
+                    </li>
+                    <li>
+                      <span>Region: </span>
+                      {region}
+                    </li>
+                    <li>
+                      <span>Sub Region: </span>
+                      {subregion}
+                    </li>
+                    <li>
+                      <span>Capital: </span>
+                      {capital}
+                    </li>
+                    </ul>
+                      </div>
+                      <div className="main-info--bottom">
+            <ul>
+                    <li>
+                      <span>Top Level Domain: </span>
+                      {topLevelDomain}
+                    </li>
+                    <li>
+                      <span>Currency: </span>
+                      {currencies[0].name}
+                    </li>
+                    <li>
+                      <span>Languages: </span>
+                      {languages.map((language) => {
+                          let list = [];
+                          list.push(`${language.name} `);
+                          return list;
+                        })}
+                    </li>
+                  </ul>
                         </div>
-                        <div className="borders-info">
-                            <span>Border Countries:</span>
-                            {borders.map((border) =>{
-                                return <span>{border}</span>
-                            })}
-                        </div>
-                    </div>
                 </div>
-        </div>
-    )
+                <div className="borders-info">
+                  <span>Border Countries:</span>
+                  {borders.map((border) => {
+                    return <span className="border">{border}</span>;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
 }
 }
 
