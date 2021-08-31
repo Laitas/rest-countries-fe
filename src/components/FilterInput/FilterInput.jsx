dafaacc394b13fc23eaee1ef5c6b41750681bc1a
Laitas <61273117+Laitas@users.noreply.github.com>
2021-08-31 12:10:42 +0300

    Finished FilterInput, updated styles

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './FilterInput.scss'
 import { AiOutlineArrowDown } from 'react-icons/ai'
-const FilterInput = ({region,setRegion}) => {
+const FilterInput = ({setRegion}) => {
   const [dropdown,setDropdown] = React.useState(false);
+  const regionChange = (e) =>{
+  setRegion(e.target.innerText)    
+  }
     return (
       <div className="filter-input">
         <span className="filter-input--title" onClick={() => setDropdown(!dropdown)}>Filter by region <AiOutlineArrowDown className='icon'/></span>
@@ -11,21 +14,13 @@ const FilterInput = ({region,setRegion}) => {
             dropdown ? "filter-input--dropdown-true" : "filter-input--dropdown-false"
           }
         >
-          <span className="option">All</span>
-          <span className="option">Africa</span>
-          <span className="option">Americas</span>
-          <span className="option">Asia</span>
-          <span className="option">Europe</span>
-          <span className="option">Oceania</span>
+          <span onClick={regionChange} className="option">All</span>
+          <span onClick={regionChange} className="option">Africa</span>
+          <span onClick={regionChange} className="option">Americas</span>
+          <span onClick={regionChange} className="option">Asia</span>
+          <span onClick={regionChange} className="option">Europe</span>
+          <span onClick={regionChange} className="option">Oceania</span>
         </div>
-        {/* <select value={region} onChange={e=>setRegion(e.target.value)}>
-            <option value=""selected disabled className='disabled'>Filter by Region</option>
-            <option value="africa">Africa</option>
-            <option value="americas">America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-        </select> */}
       </div>
     );
 }
